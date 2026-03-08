a5bc90008faecedb141acbf3d4aeedd906c0f807
Imtiaz Ahamed <imtiazzahamedd@gmail.com>
2026-03-08 17:42:41 +0600

    offcanvas datetime

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -21,3 +21,27 @@ function time() {
 
 time();
 setInterval(time, 1000);
+
+const datetimeBig = document.querySelector('.datetime-big');
+function timeBig() {
+    const now = new Date();
+    const period = getBengaliPeriod(now.getHours());
+
+    const dateStr = now.toLocaleDateString('bn-BD', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    const timeStr = now.toLocaleTimeString('bn-BD', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
+
+    datetimeBig.innerHTML = `<span class="big-time">${period} ${timeStr}</span> <br> <span class="big-date">${dateStr}</span>`;
+}
+
+
+timeBig();
+setInterval(timeBig, 1000);
